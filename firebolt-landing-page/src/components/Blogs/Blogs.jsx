5464762c9a9d8ab76ce22eff62dd7769bdecdcb4
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-30 22:52:56 +0530

    .

--- firebolt-landing-page/src/components/Blogs/Blogs.jsx
+++ firebolt-landing-page/src/components/Blogs/Blogs.jsx
@@ -8,11 +8,11 @@ function Blogs() {
             <h1 className='font-bold text-2xl text-center '>Fire-Boltt Blogs  </h1>
             <p className='text-center pt-2 text-sm'>Guidance and motivation to ignite the fire in you</p>
             <CardScrollLayout>
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/articles/Artboard_1_1_720x.jpg?v=1721629074" Description="Things You Can Do With Fire-Boltt Dapper Smartwatch" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/articles/Artboard_1_1_720x.jpg?v=1721629074" Description="Things You Can Do With Fire-Boltt Dapper Smartwatch" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/articles/Artboard_1_1_720x.jpg?v=1721629074" Description="Things You Can Do With Fire-Boltt Dapper Smartwatch" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/articles/Artboard_1_1_720x.jpg?v=1721629074" Description="Things You Can Do With Fire-Boltt Dapper Smartwatch" />
-                
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722359237/Firebolt-landing-page/rcxlyxlbcxgbmfv4ujaq.webp" Description="Fire-Boltt Onyx- What to expect from the all-in-one smartwatch?" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722359263/Firebolt-landing-page/pkbzrpsjhz4oo1bl6xkm.webp" Description="How Fire-Boltt Maverick Simplifies Your Day?" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722359266/Firebolt-landing-page/hiqih5xw7yg686bwry5v.webp" Description="Things You Can Do With Fire-Boltt Dapper Smartwatch" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722359304/Firebolt-landing-page/k5vag7qb5yry5i2fuyov.webp" Description="Explore the Universe with Fire-Boltt Spacewatch: Stainless Steel Edition" />
+
             </CardScrollLayout>
 
         </div>
