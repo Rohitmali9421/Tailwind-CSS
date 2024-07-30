61adc8cae79612d1b5acfa82d13966760d11f7a2
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-30 22:53:04 +0530

    .

--- firebolt-landing-page/src/components/Brands/Brands.jsx
+++ firebolt-landing-page/src/components/Brands/Brands.jsx
@@ -39,13 +39,13 @@ function Brands() {
     <div className=' overflow-x-clip mt-8 mx-auto max-w-[1000px]'>
       <Slider {...settings} >
         
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
-          <Card imageURL="https://www.fireboltt.com/cdn/shop/products/supernova-black_1_95x.png?v=1673500420" name="Blizzard"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355132/Firebolt-landing-page/lqbwx1tf6at3g9jry50j.avif" name="Blizzard"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355180/Firebolt-landing-page/planxexfbdvpq6gxswr2.avif" name="Asphalt"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355292/Firebolt-landing-page/kpsrdjqqlgae4unpyiro.avif" name="4g Pro"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355350/Firebolt-landing-page/ydsxhzrvbrh9uxwff5wi.avif" name="Moonwatch"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355416/Firebolt-landing-page/pfarrkymadbpl3u99xdx.avif" name="Supernova"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355480/Firebolt-landing-page/dpp9s4e7dr0d7ajj50tl.avif" name="Avalanche"/>
+          <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355535/Firebolt-landing-page/evqb7gfxe2onhwbeeuq3.webp" name="Sphere"/>
         
         
       </Slider>
--- firebolt-landing-page/src/components/Category/Category.jsx
+++ firebolt-landing-page/src/components/Category/Category.jsx
@@ -7,24 +7,24 @@ function Category() {
         <h1 className='font-bold text-2xl md:text-4xl '>Shop By  </h1>
         <h1 className='font-bold text-2xl md:text-4xl pl-2 text-orange-600'>Categories</h1>
       </div>
-      <div className='grid w-full px-3  gap-3 grid-cols-3 md:grid-cols-4 mt-8'>
+      <div className='grid w-full px-3  md:gap-3 gap-1 grid-cols-3 md:grid-cols-4 mt-8'>
         <div className='col-span-3  md:col-span-2'>
-          <img src="https://www.fireboltt.com/cdn/shop/files/Cellular_calling_9f3eb9ed-fcac-4d9d-bffa-598421d588c9_1024x1024.jpg?v=1721281545" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357518/Firebolt-landing-page/nhnkloc5fkdgf16nzhsx.webp" className=' object-cover h-full' alt="" />
         </div>
         <div className='  '>
-          <img src="https://www.fireboltt.com/cdn/shop/files/luxe_edition_1024x1024.jpg?v=1720816544" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357609/Firebolt-landing-page/xc2rfykpqewxmyl6vbmf.webp" className=' object-cover h-full' alt="" />
         </div>
         <div className='  row-span-2'>
-          <img src="https://www.fireboltt.com/cdn/shop/files/Bt_Calling_1_1024x1024.jpg?v=1721974891" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357657/Firebolt-landing-page/imcnxpqhwhlfncafdncg.webp" className=' object-cover h-full' alt="" />
         </div>
         <div className=' '>
-          <img src="https://www.fireboltt.com/cdn/shop/files/outdoor_series_1024x1024.jpg?v=1720816561" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357710/Firebolt-landing-page/bnauhs4urqk8fknow3s7.webp" className=' object-cover h-full' alt="" />
         </div>
         <div className=' '>
-          <img src="https://www.fireboltt.com/cdn/shop/files/amoled_display_10e57672-7a30-45da-9f2a-62b453d10a3c_1024x1024.jpg?v=1720816578" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357781/Firebolt-landing-page/gzavetswypjseaa8qsxf.webp" className=' object-cover h-full' alt="" />
         </div>
         <div className=' '>
-          <img src="https://www.fireboltt.com/cdn/shop/files/Audio_Series_1024x1024.jpg?v=1720816592" className=' object-cover h-full' alt="" />
+          <img src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722357820/Firebolt-landing-page/gmc1oqgslp01zdusq2a1.webp" className=' object-cover h-full' alt="" />
         </div>
       </div>
     </div>
