fdd86dc934a00fd6c488b347516e004ea859736a
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-30 22:53:08 +0530

    .

--- firebolt-landing-page/src/components/Deals/Deals.jsx
+++ firebolt-landing-page/src/components/Deals/Deals.jsx
@@ -10,10 +10,10 @@ function Deals() {
                 <h1 className='font-bold text-2xl md:text-4xl text-orange-600'>Deals</h1>
             </div>
             <CardScrollLayout>
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358357/Firebolt-landing-page/m2qnlj8a16zw7p0gvcbi.webp" name="Dream " description="Android | Cellular Calling " price="₹4,499" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358413/Firebolt-landing-page/kzzpxdy801tkwd3qcxqe.webp" name="Case X series " description="49.5mm Display | BT Calling  " price="₹2,499" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358425/Firebolt-landing-page/owbx6wmpcvqis6pwqngc.webp" name="Oracle  " description="Android | Cellular Calling  " price="₹3,999" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358473/Firebolt-landing-page/perb90zdtlj4s1dcalm5.webp" name="4G Pro   " description="55mm Display | VoLTE " price="₹2,499" />
             </CardScrollLayout>
 
         </div>
--- firebolt-landing-page/src/components/Features/Features.jsx
+++ firebolt-landing-page/src/components/Features/Features.jsx
@@ -10,7 +10,7 @@ function Features() {
             <div className='grid  md:grid-cols-4 grid-cols-2 gap-4 mt-10 mx-4'>
                 <div className='w-full bg-gray-100 rounded-md  border-2 flex justify-center py-3'>
                     <div className='flex gap-3 '>
-                        <img className='h-14' src="https://www.fireboltt.com/cdn/shop/files/Unit_1_100x.gif?v=1720089586" alt="" />
+                        <img className='h-14' src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355630/Firebolt-landing-page/ic5lcp0hknvmtk89sdfl.webp" alt="" />
                         <div>
                             <h1 className='text-xl font-bold '>20Mn+</h1>
                             <h1 className='text-gray-600 font-semibold  text-xs font-mono'>UNITS SOLD</h1>
@@ -19,7 +19,7 @@ function Features() {
                 </div>
                 <div className='w-full bg-gray-100 rounded-md  border-2 flex justify-center py-3'>
                     <div className='flex gap-3 '>
-                        <img className='h-14' src="https://www.fireboltt.com/cdn/shop/files/Review_1_100x.gif?v=1720089618" alt="" />
+                        <img className='h-14' src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355683/Firebolt-landing-page/rnkolrx5zt3rflwvw4mf.webp" alt="" />
                         <div>
                             <h1 className='text-xl font-bold '>02Mn+</h1>
                             <h1 className='text-gray-600 font-semibold  text-xs font-mono'>PRODUCT REVIEWS</h1>
@@ -28,7 +28,7 @@ function Features() {
                 </div>
                 <div className='w-full bg-gray-100 rounded-md  border-2 flex justify-center py-3'>
                     <div className='flex gap-3 '>
-                        <img className='h-14' src="https://www.fireboltt.com/cdn/shop/files/Graph_100x.gif?v=1720084637" alt="" />
+                        <img className='h-14' src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355744/Firebolt-landing-page/iyijxuaspvjttodhfuxr.webp" alt="" />
                         <div>
                             <h1 className='text-xl font-bold '>100% YOY</h1>
                             <h1 className='text-gray-600 font-semibold  text-xs font-mono'>400% QOQ GROWTH</h1>
@@ -37,7 +37,7 @@ function Features() {
                 </div>
                 <div className='w-full bg-gray-100 rounded-md  border-2 flex justify-center py-3'>
                     <div className='flex gap-3 '>
-                        <img className=' h-14' src="https://www.fireboltt.com/cdn/shop/files/Timer_1_2_100x.gif?v=1720074184" alt="" />
+                        <img className=' h-14' src="https://res.cloudinary.com/dhturqqs5/image/upload/v1722355780/Firebolt-landing-page/zlsyuqlpam2yye9ibkdu.webp" alt="" />
                         <div>
                             <h1 className='text-xl font-bold '>1 UNIT SOLD </h1>
                             <h1 className='text-gray-600 font-semibold  text-xs font-mono'>EVERY 05 SEC</h1>
