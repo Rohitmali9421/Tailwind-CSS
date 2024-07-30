9253876df17c9ae38ae31111870a6714dee03dc3
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-30 22:52:53 +0530

    .

--- firebolt-landing-page/src/components/Bestsellers/Bestsellers.jsx
+++ firebolt-landing-page/src/components/Bestsellers/Bestsellers.jsx
@@ -41,13 +41,16 @@ function Bestsellers() {
                 <h1 className='font-bold text-2xl md:text-4xl '>Explore </h1>
                 <h1 className='font-bold text-2xl md:text-4xl pl-2 text-orange-600'>Bestsellers</h1>
             </div>
-            <div className=' overflow-x-clip mt-8 mx-4 '>
+            <div className=' overflow-x-clip mt-8 md:mx-4 mx-1 '>
                 <Slider {...settings} >
 
-                    <Card imageURL="https://cdn.shopify.com/videos/c/o/v/0666fccb5f39493d9af9aa27f483a23a.mp4" name="Dream" />
-                    <Card imageURL="https://cdn.shopify.com/videos/c/o/v/3275ef01f7464bc38ce58ea1c5dd83df.mp4" name="Retro" />
-                    <Card imageURL="https://cdn.shopify.com/videos/c/o/v/cc0b7c544ec34dd3a5285f0af8149ee8.mp4" name="Royal" />
-                    <Card imageURL="https://cdn.shopify.com/videos/c/o/v/8196b82a14274afe8a910b08d5ec5e4c.mp4" name="Blizzard" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722356727/Firebolt-landing-page/w6bakleokqh0vdoveet3.mp4" name="Dream" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722356819/Firebolt-landing-page/a9yeqg2yjvmhh8a0bips.mp4" name="Retro" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722356897/Firebolt-landing-page/hk8rf0lntv9cu197v14u.mp4" name="Blizzard Ultra" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722356971/Firebolt-landing-page/sc0r9t2eta4acgc2uwti.mp4" name="Royale" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722357085/Firebolt-landing-page/tzhq6x3k9s8dg1qzoupv.mp4" name="Blizzard" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722357202/Firebolt-landing-page/g5fvspbflhl2kpnpcnnj.mp4" name="Supernova" />
+                    <Card imageURL="https://res.cloudinary.com/dhturqqs5/video/upload/v1722357306/Firebolt-landing-page/si2jrekpdithbrxufi5s.mp4" name="Solaris" />
 
                 </Slider>
             </div>
