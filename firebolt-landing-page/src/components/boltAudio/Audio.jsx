4d155f6ce536039273fb46af2a494a72ef0874f0
Rohit Mali <rohitsmali9421@gmail.com>
2024-07-30 22:52:59 +0530

    .

--- firebolt-landing-page/src/components/boltAudio/Audio.jsx
+++ firebolt-landing-page/src/components/boltAudio/Audio.jsx
@@ -12,10 +12,10 @@ function Deals() {
 
             </div>
             <CardScrollLayout>
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
-                <Card imageURL="https://www.fireboltt.com/cdn/shop/files/Black_01_53a058b5-640b-450b-af00-901dff5815e6_540x.png?v=1720530551" name="Maverick" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358806/Firebolt-landing-page/fj6p8h3rahff0acirtce.webp" name="Fire Band Athena  " description="10mm Driver | ENC Mic  " price="₹799" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358838/Firebolt-landing-page/bcwkkfgo2waqzhnxk4r4.webp" name="FirePods Antares " description="10mm Driver | Game Mode " price="₹1,299" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358849/Firebolt-landing-page/lvuzz9bwisdgptfdbtcv.webp" name="Lycan GamePods " description="IPX5 Quad | ENC Mics   " price="₹999" />
+                <Card imageURL="https://res.cloudinary.com/dhturqqs5/image/upload/v1722358870/Firebolt-landing-page/abf6jjjfrogwytz8uq2y.webp" name="Fire Band Nebula " description="14.2mm Driver | IPX5 " price="₹899" />
             </CardScrollLayout>
 
         </div>
