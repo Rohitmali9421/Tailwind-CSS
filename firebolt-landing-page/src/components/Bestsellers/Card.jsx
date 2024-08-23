eb5344123d612f81dd6d6f25e26ca0eb72bf8779
Rohit Mali <rohitsmali9421@gmail.com>
2024-08-23 18:22:03 +0530

    .

--- firebolt-landing-page/src/components/Bestsellers/Card.jsx
+++ firebolt-landing-page/src/components/Bestsellers/Card.jsx
@@ -4,7 +4,7 @@ function Card({ imageURL, name }) {
     return (
         <div className='min-w-60    mx-auto'>
             <div className='mx-3 '>
-            <video className='rounded-xl border border-orange-500' src={imageURL} autoPlay playsInline muted loop preload="none"></video>
+            <video className='rounded-xl border border-orange-500'  src={imageURL} autoPlay playsInline muted loop preload="none"></video>
                 <h1 className='text-center font-bold pt-2 text-2xl'>{name}</h1>
             </div>
         </div>
--- firebolt-landing-page/src/components/Blogs/Card.jsx
+++ firebolt-landing-page/src/components/Blogs/Card.jsx
@@ -4,7 +4,7 @@ function Card({ imageURL, Description }) {
     return (
         <div className='min-w-60      mx-auto'>
             <div className='mx-3  rounded-xl'>
-                <img className=' rounded-xl h-48' src={imageURL} alt="" />
+                <img className=' rounded-xl h-48' loading="lazy" src={imageURL} alt="" />
                 <p className='text-xl mt-4 font-bold '>{Description}</p>
                 
 
